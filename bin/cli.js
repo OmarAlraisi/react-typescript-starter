de0def4e7203631be7eb728770a8e02009b897b7
AlRaisi96 <o.raisi@hotmail.com>
2022-12-14 11:59:30 +0400

    On "npx react-typescript-starter app-name" does not clone the master branch which includes "/bin"

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 import { execSync } from "child_process";
+import { existsSync } from "fs";
+
 const projectName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/OmarAlraisi/react-typescript-starter.git ${projectName}`;
+const gitCheckoutCommand = `git clone --branch project-starter https://github.com/OmarAlraisi/react-typescript-starter.git ${projectName}`;
 const installDependencies = `cd ${projectName} && npm install`;
+
+if (existsSync(projectName)) {
+  console.log(`Error: "${projectName}" already exists.`);
+  process.exit(-1);
+}
 
 const runCommand = (command) => {
   try {
